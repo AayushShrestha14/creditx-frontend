211cdede44797b214c8c125d33c355c3c5d5b5f9
Paribartan Kalathoki <paribartan.kalathoki@sbsolutionsnepal.com>
2022-03-03 11:48:35 +0545

    seperate dashboard modules

--- src/app/feature_modules/feature-routing.module.ts
+++ src/app/feature_modules/feature-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '../core/guards/auth-gaurd';
-import { DashboardComponent } from './dashboard_modules/dashboard/dashboard.component';
+import { DashboardComponent } from '../dashboard_modules/dashboard/dashboard.component';
 import { FeatureModulesComponent } from './feature-modules.component';
 
 const routes: Routes = [
@@ -9,7 +9,6 @@ const routes: Routes = [
     path: '', component: FeatureModulesComponent,
     children: [
       {path: 'dashboard', component: DashboardComponent},
-      
       {
         path: 'admin_modules',
         loadChildren: () => import('./admin_modules/admin.module').then(m => m.AdminModule),
