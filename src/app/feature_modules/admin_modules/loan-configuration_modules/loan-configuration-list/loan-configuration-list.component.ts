efe8ee49399ce3af02f6cdc62bfd45b151d22f63
unishbhattarai7 <unishbhattarai7@gmail.com>
2022-03-07 12:32:56 +0545

    feature/loan-configuration

---
+++ src/app/feature_modules/admin_modules/loan-configuration_modules/loan-configuration-list/loan-configuration-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-configuration-list',
+  templateUrl: './loan-configuration-list.component.html',
+  styleUrls: ['./loan-configuration-list.component.scss']
+})
+export class LoanConfigurationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
