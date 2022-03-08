2e7e88a5c2c94ccfeca1151fff4888cf0ee702ce
Ramesh Kumar Bhandari <ramesh152200@gmail.com>
2022-03-08 10:39:58 +0545

    created valuator component and designed header and table part

--- src/app/feature_modules/admin_modules/admin.module.ts
+++ src/app/feature_modules/admin_modules/admin.module.ts
@@ -8,6 +8,9 @@ import { ThemeModule } from 'src/app/theme_modules/theme.module';
 import { CommonResourceModule } from 'src/app/common_resource_modules/common-resource.module';
 import { HeaderActionComponent } from './roles_permissions_modules/action-component/header-action/header-action.component';
 import { RolesActionComponent } from './roles_permissions_modules/action-component/table-action/roles-action.component';
+import { ValuatorListComponent } from './vauator_modules/valuator-list/valuator-list.component';
+import { TableActionComponent } from './vauator_modules/action-component/table-action/table-action.component';
+import { ValuatorHeaderActionComponent } from './vauator_modules/action-component/header-action/header-action.component';
 
 
 @NgModule({
@@ -15,17 +18,22 @@ import { RolesActionComponent } from './roles_permissions_modules/action-compone
     BranchComponent,
     RolesPermissionsListComponent,
     RolesActionComponent,
-    HeaderActionComponent
+    HeaderActionComponent,
+    ValuatorListComponent,
+    TableActionComponent,
+    ValuatorHeaderActionComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     ThemeModule.forChild(),
     CommonResourceModule
-  ], 
+  ],
   exports: [
     RolesActionComponent,
-    HeaderActionComponent
+    HeaderActionComponent,
+    TableActionComponent,
+    ValuatorHeaderActionComponent,
   ]
 })
 export class AdminModule { }
---
+++ src/app/feature_modules/admin_modules/vauator_modules/models/valuator-response.model.ts
@@ -0,0 +1,18 @@
+import { SbBaseModel } from 'src/app/common_resource_modules/models/sb-base.model';
+
+export class ValuatorResponse extends SbBaseModel {
+  name: string | undefined;
+  province: string | undefined;
+  district: string | undefined;
+  municipalityVdc: string | undefined;
+  streetName: string | undefined;
+  wardNumber: number | undefined;
+  contactNo: string | undefined;
+  email: string | undefined;
+  branch: string | undefined;
+  valuatingField: string | undefined;
+  bankAssociateDate: string | undefined;
+  minAmount: string | undefined;
+  maxAmount: string | undefined;
+  status: string | undefined;
+}
